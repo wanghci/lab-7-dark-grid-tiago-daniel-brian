8b61d0a596d1f42a4a312c7c9cf9dfbea8f24b25
TiagoSilva4 <tiagos3373@gmail.com>
2024-02-29 15:19:07 -0500

    asdf

---
+++ script.js
@@ -0,0 +1,13 @@
+let main = document.getElementById("main");
+let def = document.getElementById("def");
+thing.addEventListener("click", () => {
+    main.classList.add;
+})
+let light = document.getElementById("light");
+thing.addEventListener("click", () => {
+    main.classList.toggle("light");
+})
+let dark = document.getElementById("dark");
+thing.addEventListener("click", () => {
+    main.classList.toggle("dark");
+})
